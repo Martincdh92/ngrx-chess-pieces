8afff6abe29025713c7caa47151e17d517010796
Martin Contreras <martincdh92@gmail.com>
2022-08-13 13:00:22 -0500

    Adding initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { ShowCaseComponent } from './components/show-case/show-case/show-case.component';
+import { CartComponent } from './components/cart/cart/cart.component';
+import { ItemBriefComponent } from './components/item-brief/item-brief/item-brief.component';
+import { ItemDetailedComponent } from './components/item-detailed/item-detailed/item-detailed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowCaseComponent,
+    CartComponent,
+    ItemBriefComponent,
+    ItemDetailedComponent
   ],
   imports: [
     BrowserModule,
